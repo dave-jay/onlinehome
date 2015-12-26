1b31f634fd3ffdf16f3dc5f14076125295fd2320
dave jay <dave.jay90@gmail.com>
2015-12-26 18:32:36 +0530

    call to agents

---
+++ instance/front/controller/AgentCallLog.inc.php
@@ -0,0 +1,8 @@
+<?php
+$urlArgs = _cg("url_vars");
+$dealId = $_SESSION['dealId'];
+    //$agent_numbers = $_REQUEST['agent_no'];
+    //$dealId = $_REQUEST['dealId'];
+    
+   
+die; ?>
---
+++ instance/front/controller/ConnectingAgent.inc.php
@@ -0,0 +1,27 @@
+<?php
+    //$agent_numbers = explode(',', $_REQUEST['agent_numbers']);
+    //$dealId = $_REQUEST['dealId'];
+    $agent_numbers = explode(',', $_SESSION['agent_numbers']);
+    $dealId = $_SESSION['dealId'];
+    
+    // if the caller pressed anything but 1 send them back
+    //if($_REQUEST['Digits'] != '1') {
+    //    header("Location: http://s606346885.onlinehome.us/test_dial4");
+        //header("Location: http://localhost/twilio-call-text/test_dial4");
+    //    die;
+    //}
+    
+    // the user pressed 1, connect the call to 310-555-1212 
+    header("content-type: text/xml");
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    // <Dial>+18774942065</Dial>
+?>
+<Response>
+    <Dial>
+        <?php foreach($agent_numbers as $key => $value): ?>
+        <Number url="http://s606346885.onlinehome.us/AgentCallLog/<?php print $value; ?>">+<?php print $value; ?></Number>
+        <?php endforeach; ?>
+    </Dial>
+    <Say>The call failed or the remote party hung up. Goodbye.</Say>
+</Response>
+<?php die; ?>
