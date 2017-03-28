adb0f65e544b3bf0e12b18f9c7ba08cd675ebe65
hardik-panchal <hardik@go-brilliant.com>
2017-03-28 12:17:20 +0530

    lysoft Agent List Modal Set

--- instance/front/controller/agents.inc.php
+++ instance/front/controller/agents.inc.php
@@ -1,38 +1,44 @@
 <?php
-if($_REQUEST['syncUser']){
-    include _PATH.'instance/front/controller/schedulerGetDealUsers.inc.php';
+
+if ($_REQUEST['syncUser']) {
+    include _PATH . 'instance/front/controller/schedulerGetDealUsers.inc.php';
     die;
 }
-if($_REQUEST['MarkAsDefault']){
-    qu("pd_users",array("is_default"=>0),"1=1");
-    qu("pd_users",array("is_default"=>1),"id='{$_REQUEST['user_id']}'");
+if ($_REQUEST['MarkAsDefault']) {
+    qu("pd_users", array("is_default" => 0), "1=1");
+    qu("pd_users", array("is_default" => 1), "id='{$_REQUEST['user_id']}'");
     $agents = q("select * From pd_users where is_active='1' order by name asc ");
-    include _PATH.'instance/front/tpl/agents_data.php';
+    include _PATH . 'instance/front/tpl/agents_data.php';
     die;
 }
-if($_REQUEST['RemoceFromDefault']){
-    qu("pd_users",array("is_default"=>0),"id='{$_REQUEST['user_id']}'");
+if ($_REQUEST['RemoceFromDefault']) {
+    qu("pd_users", array("is_default" => 0), "id='{$_REQUEST['user_id']}'");
     $agents = q("select * From pd_users where is_active='1' order by name asc ");
-    include _PATH.'instance/front/tpl/agents_data.php';
+    include _PATH . 'instance/front/tpl/agents_data.php';
     die;
 }
-if($_REQUEST['updateUser']){
+if ($_REQUEST['updateUser']) {
     $agents = q("select * From pd_users where is_active='1' order by name asc ");
-    include _PATH.'instance/front/tpl/agents_data.php';
+    include _PATH . 'instance/front/tpl/agents_data.php';
     die;
 }
 if ($_REQUEST['doUpdateContact']) {
     $agent_id = _escape($_REQUEST['doUpdateContact']);
+   
     $phone = _escape(trim($_REQUEST['phone']));
     $cell = _escape(trim($_REQUEST['cell']));
+    $role = _escape(trim($_REQUEST['role']));
+    $linkdin = _escape(trim($_REQUEST['linkdin']));
     $group = _escape(trim($_REQUEST['group']));
+//     d($linkdin);
+//    die;
     $affected_row = -1;
-    /*if ($phone || $cell) {
-        $affected_row = qu('pd_users', array("phone" => $phone,"cell" => $cell,"group" => $group), " id = '{$agent_id}'  ");
-    }else{
-        $affected_row=0;
-    }*/
-    $affected_row = qu('pd_users', array("phone" => $phone,"cell" => $cell,"group" => $group), " id = '{$agent_id}'  ");
+    /* if ($phone || $cell) {
+      $affected_row = qu('pd_users', array("phone" => $phone,"cell" => $cell,"group" => $group), " id = '{$agent_id}'  ");
+      }else{
+      $affected_row=0;
+      } */
+    $affected_row = qu('pd_users', array("phone" => $phone, "cell" => $cell, "group" => $group, "role" => $role, "linkedin_link" => $linkdin), " id = '{$agent_id}'  ");
     echo $affected_row;
     die;
 }
